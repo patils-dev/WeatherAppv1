4a6b8c813001407716d8f6931e266da24a5c27cc
krushnap <smitakpatil26@gmail.com>
2019-10-03 22:16:48 +0530

    http to https error

--- try.js
+++ try.js
@@ -1,15 +1,4 @@
-// let weather;
 
-// $(document).ready(function(){
-//     $.getJSON("http://api.openweathermap.org/data/2.5/find?q=kolhapur&units=metric&appid=344b790f76ac39d29c9973f2736c19c3", 
-//     function(data){
-//     console.log(data);
-//     weather=data;
-//     let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
-//         console.log(icon);
-//     })
-//     display();
-// })
 let temp1;
 document.getElementById("list").onkeypress = function(event){
     if (event.keyCode == 13 || event.which == 13){
@@ -21,30 +10,24 @@ function displayData(){
     let result=document.getElementById("list").value;
     $.getJSON("http://api.openweathermap.org/data/2.5/find?q="+result+"&units=metric&appid=344b790f76ac39d29c9973f2736c19c3", 
     function(data){
-        
+
         let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
         document.getElementById("cityName").innerHTML=data.list[0].name;
         document.getElementById("stateName").innerHTML=data.list[0].sys.country;
         $("#icon").attr("src",icon);
         document.getElementById("weatherName").innerHTML=data.list[0].weather[0].main;
-
         document.getElementById("temperature").innerHTML=data.list[0].main.temp;
-
         document.getElementById("celcius").disabled = true;
     })
 }
-// $(document).ready(){
 
-// }
 document.getElementById("fahrenheit").addEventListener("click",function displayFah(e){
     let temp1=document.getElementById("temperature").textContent;
     console.log(temp1);
-   
 
-            let fahTemp=Math.round((temp1*1.8)+32);
-            // console.log(fahTemp);
-            document.getElementById("temperature").innerHTML=fahTemp;
-   
+    let fahTemp=Math.round((temp1*1.8)+32);
+    // console.log(fahTemp);
+    document.getElementById("temperature").innerHTML=fahTemp;
     document.getElementById("fahrenheit").style.backgroundColor = "rgb(178, 183, 189)";
     document.getElementById("fahrenheit").disabled = true;
     document.getElementById("celcius").disabled = false;
@@ -66,11 +49,9 @@ document.getElementById("celcius").addEventListener("click",function displayCel(
 
 
 $(document).ready(function(){
-    
         var d=new Date().toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' });
         var newD = new Date();
         var n = newD.getDay()
         let week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
         document.getElementById("day").innerHTML = week[n]+" , "+d;
-    
 })
