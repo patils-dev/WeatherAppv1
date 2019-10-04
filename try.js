062cc9b894e2a68bd71f02198e9b818f531046e3
krushnap <smitakpatil26@gmail.com>
2019-10-04 10:25:33 +0530

    http error

--- try.js
+++ try.js
@@ -8,7 +8,8 @@ document.getElementById("list").onkeypress = function(event){
 function displayData(){
 
     let result=document.getElementById("list").value;
-    $.getJSON("http://api.openweathermap.org/data/2.5/find?q="+result+"&units=metric&appid=344b790f76ac39d29c9973f2736c19c3", 
+    const proxy="https://cors-anywhere.herokupp.com/"
+    $.getJSON(`${proxy}http://api.openweathermap.org/data/2.5/find?q="+result+"&units=metric&appid=344b790f76ac39d29c9973f2736c19c3`, 
     function(data){
 
         let icon="http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
