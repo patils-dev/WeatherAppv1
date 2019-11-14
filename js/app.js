687427ba943b4792ee8d5bf202f6b9829d4f9fa0
smita patil <smitakpatil26@gmail.com>
2019-11-14 22:57:35 +0530

    added comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-//display city details
+//this function will excecute after pressing enter key and it will get the data which is entered.
 document.getElementById("list").addEventListener("keypress",function displayData(e){
     if(e.keyCode==13)
     {
-        // console.log("kegjd")
         let result=document.getElementById("list").value;
         dw =new Weather(result);
         dw.displayValues();
@@ -10,17 +9,15 @@ document.getElementById("list").addEventListener("keypress",function displayData
 
 })
 
-// display temperature in Fahrenheit
+//this function will excecute after clicking on farenheit button and it will convert temperature in Fahrenheit unit.
 document.getElementById("fahrenheit").addEventListener("click",function displayFah(e){
-
         let result=document.getElementById("list").value;
-    
         dw =new Weather(result);
         dw.displayFahrenheit();
 })
- //display temperature in celcius
-document.getElementById("celcius").addEventListener("click",function displayCel(e){
-    
+
+//this function will excecute after clicking on celcius button and it will convert temperature in Celcius unit.
+document.getElementById("celcius").addEventListener("click",function displayCel(e){ 
     let result=document.getElementById("list").value;
     dw =new Weather(result);
     dw.displayCelcius();
